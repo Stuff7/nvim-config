bedcc6ea0a513b4fc676cf1a1f7942adbadec23d
Armando M <cuentafrst@gmail.com>
2023-10-21 20:55:05 -0600

    theme lsp colors

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -19,32 +19,50 @@ require("bufferline").setup({
 require("nvim-tree").setup()
 
 -- Colorscheme
-local c = require("vscode.colors").get_colors()
 require("vscode").setup({
 	transparent = true,
 	italic_comments = true,
 	disable_nvimtree_bg = true,
 })
+local c = {
+  blue = "#569CD6",
+  blueGreen = "#4EC9B0",
+  accentBlue = "#4FC1FE",
+  pink = "#FF8FFF",
+  orange = "#FF7F00",
+  grey = "#808080",
+  purple = "#998CFF",
+  palePurple = "#C5B6C0",
+  darkPalePurple = "#A596B9",
+  paleGreen = "#C0FED8",
+  mediumGreen = "#40A940",
+  darkAquamarine = "#2FAF84",
+  paleAquamarine = "#93CDB9",
+  yellow = "#FFFF00",
+}
 vim.api.nvim_create_autocmd('ColorScheme', {
   callback = function ()
-    vim.api.nvim_set_hl(0, "@lsp.type.comment", { fg = "#808080" })
-    vim.api.nvim_set_hl(0, "@comment", { fg = "#808080" })
-    vim.api.nvim_set_hl(0, "@lsp.mod.mutable", { fg = "#A6ACF6", bold = true })
+    vim.api.nvim_set_hl(0, "@lsp.type.comment", { fg = c.grey })
+    vim.api.nvim_set_hl(0, "@comment", { fg = c.grey })
+    vim.api.nvim_set_hl(0, "@lsp.mod.mutable", { fg = c.purple, bold = true })
     vim.api.nvim_set_hl(0, "@lsp.mod.reference", { italic = true })
-    vim.api.nvim_set_hl(0, "@lsp.typemod.deriveHelper.attribute", { fg = "#C5B6C0" })
-    vim.api.nvim_set_hl(0, "@lsp.typemod.variable.constant", { fg = c.vscAccentBlue })
-    vim.api.nvim_set_hl(0, "@lsp.typemod.selfKeyword.mutable", { fg = "#F6AFF6" })
-    vim.api.nvim_set_hl(0, "@lsp.type.selfKeyword", { fg = c.vscBlue })
-    vim.api.nvim_set_hl(0, "@lsp.type.selfTypeKeyword", { fg = c.vscBlue })
-    vim.api.nvim_set_hl(0, "@lsp.type.decorator", { fg = "#A596B9" })
-    vim.api.nvim_set_hl(0, "@lsp.type.parameter", { fg = "#C0FED8" })
-    vim.api.nvim_set_hl(0, "@lsp.type.interface", { fg = "#40A940", italic = true })
-    vim.api.nvim_set_hl(0, "@lsp.type.struct", { fg = "#8EC9B0" })
-    vim.api.nvim_set_hl(0, "@lsp.type.enum", { fg = "#00A980" })
-    vim.api.nvim_set_hl(0, "@lsp.type.formatSpecifier", { fg = c.vscBlue })
-    vim.api.nvim_set_hl(0, "@lsp.type.builtinType", { fg = c.vscBlueGreen })
-    vim.api.nvim_set_hl(0, "@lsp.type.macro", { fg = c.vscBlue })
-    vim.api.nvim_set_hl(0, "@function.macro", { fg = c.vscBlue })
+    vim.api.nvim_set_hl(0, "@lsp.typemod.deriveHelper.attribute", { fg = c.palePurple })
+    vim.api.nvim_set_hl(0, "@lsp.typemod.variable.constant", { fg = c.accentBlue })
+    vim.api.nvim_set_hl(0, "@lsp.typemod.method.mutable", { fg = c.yellow })
+    vim.api.nvim_set_hl(0, "@lsp.typemod.selfKeyword.reference", { fg = c.blue })
+    vim.api.nvim_set_hl(0, "@lsp.typemod.selfKeyword.mutable", { fg = c.pink })
+    vim.api.nvim_set_hl(0, "@lsp.typemod.selfKeyword.declaration", { fg = c.orange })
+    vim.api.nvim_set_hl(0, "@lsp.type.selfKeyword", { fg = c.blue })
+    vim.api.nvim_set_hl(0, "@lsp.type.selfTypeKeyword", { fg = c.blue })
+    vim.api.nvim_set_hl(0, "@lsp.type.decorator", { fg = c.darkPalePurple })
+    vim.api.nvim_set_hl(0, "@lsp.type.parameter", { fg = c.paleGreen })
+    vim.api.nvim_set_hl(0, "@lsp.type.interface", { fg = c.mediumGreen, italic = true })
+    vim.api.nvim_set_hl(0, "@lsp.type.struct", { fg = c.paleAquamarine })
+    vim.api.nvim_set_hl(0, "@lsp.type.enum", { fg = c.darkAquamarine })
+    vim.api.nvim_set_hl(0, "@lsp.type.formatSpecifier", { fg = c.blue })
+    vim.api.nvim_set_hl(0, "@lsp.type.builtinType", { fg = c.blueGreen })
+    vim.api.nvim_set_hl(0, "@lsp.type.macro", { fg = c.blue })
+    vim.api.nvim_set_hl(0, "@function.macro", { fg = c.blue })
   end
 })
 vim.cmd.colorscheme "vscode"
