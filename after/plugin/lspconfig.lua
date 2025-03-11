8fde17d732ea3fe284c375ef3257e71a69944711
Stuff7 <cuentafrst@gmail.com>
2025-03-10 19:44:05 -0600

    biomejs

--- after/plugin/lspconfig.lua
+++ after/plugin/lspconfig.lua
@@ -36,6 +36,10 @@ local function get_typescript_server_path(root_dir)
   end
 end
 
+lspconfig.biome.setup{
+  root_dir = util.root_pattern("package.json"),
+}
+
 lspconfig.volar.setup{
   on_new_config = function(new_config, new_root_dir)
     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
