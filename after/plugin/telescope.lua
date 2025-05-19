045b3206ffce141d068c90cd573788b068ea1f01
Stuff7 <cuentafrst@gmail.com>
2025-05-18 22:05:22 -0600

    ignore vendor

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,10 @@
+local telescope = require("telescope")
 local builtin = require("telescope.builtin")
+telescope.setup{
+  defaults = {
+    file_ignore_patterns = { "vendor/" }
+  }
+}
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>ps", function()
