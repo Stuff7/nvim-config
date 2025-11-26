04f543e29c315df77b22b752e0b42b3ebace9bdf
Stuff7 <cuentafrst@gmail.com>
2025-11-26 02:58:04 -0600

    sql for any zig strings

--- after/queries/zig/injections.scm
+++ after/queries/zig/injections.scm
@@ -1,8 +1,7 @@
 ; extends
 
 ; SQL syntax highlighting in strings
-(call_expression
-  function: (field_expression
-    member: (identifier) @_method)
+(
   [(multiline_string) @injection.content (string (string_content) @injection.content)]
-  (#set! injection.language "sql"))
+  (#set! injection.language "sql")
+)
